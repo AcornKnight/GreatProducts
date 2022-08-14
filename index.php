0595af15b9ac9764a4ee6467af2012fc6302ad49
Ken <ken_chief_engineer@yahoo.com>
2022-08-14 19:22:00 -0400

    Implement product by category view in main page

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once('settings.php');
 
+// "By Category" Can be manually driven until wired up in the interface with:
+// http://localhost/GreatProducts/index.php?CategoryID=1
+
 if($user) {
     echo 'Hello '.$user['Username'];
     echo '<a href="profile.php">Profile</a>';
@@ -8,9 +11,16 @@ if($user) {
 } else {
     echo '<a href="login.php">Login</a>';
 }
-
+echo '<hr />';
+$selStatement = 'SELECT * FROM products';
+if (isset($_GET['CategoryID'])) {
+    $selStatement .= ' WHERE ProductID IN (SELECT ProductID FROM ProductCategory WHERE CatID = ' . $_GET['CategoryID'] . ')';
+    $cat = $db->query('SELECT CatName FROM category WHERE CatID = '.$_GET['CategoryID']);
+    $cat = $cat->fetch();
+    echo '<h3> Products in category: ' . $cat['CatName'] . '</h3><hr />';
+}
 // READ ALL
-$products=$db->query('SELECT * FROM products');
+$products=$db->query($selStatement);
 echo '<table border="1">';
 while($row=$products->fetch()){
 //    print_r($row);
