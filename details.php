aa648a6bcc727d85b04f3942924afae4cee5afdf
Ken <ken_chief_engineer@yahoo.com>
2022-08-14 18:48:45 -0400

    Details page with working add to cart button is user logged in

--- details.php
+++ details.php
@@ -1,23 +1,23 @@
 <?php
 require_once('settings.php');
 
-print_r($_GET);
+//print_r($_GET);
 $prod_id = $_GET['ProductID'];
 if($prod_id) {
     $p = $db->query('SELECT * FROM products WHERE ProductID =' . $_GET['ProductID']);
     $product = $p->fetch();
-    print_r($product);
+//    print_r($product);
     echo '<h3>'.$product["Name"].'</h3>';
     echo '<hr /><p>'.$product["Details"].'</p>';
     echo '<hr /><h3>'.$product["Cost"].'</h3>';
-    if(true) {
-        echo '<h1>HOwdy</h1><form action="add2cart.php?ProductID="'.$prod_id.
-            '<input type="submit" value="Add to Cart">
-        </form>';
+    if($user) {
+        echo'<form action="add2cart.php?ProductID='.$prod_id.'">'.
+            '<input type="submit" value="Add to Cart">'.
+        '</form>';
     }
 } else {
     echo '<script type=javascript>alert("ProductID not found");</script>';
 }
 
-echo '<form action="/index.php"><input type="submit" value="CLOSE"></form>';
+echo '<hr /><a href="./index.php">CLOSE</a>';
 
