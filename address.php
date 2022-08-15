0acbd73a45bebede05bed823f90587da6825d4c8
Ken <ken_chief_engineer@yahoo.com>
2022-08-15 00:23:16 -0400

    updates for Address CRUD visuals

--- address.php
+++ address.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<!-- Address CRUD support file for our Great products database. -->
+<!-- Noah R Gestiehr. -->
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
 <?php
 require_once('settings.php');
 
@@ -8,24 +20,30 @@ if(isset($_GET['action'])) {
         $db->query('DELETE FROM Address where AddressID = ' . $_GET['AddressID']);
         header('Location: profile.php');
     } else if ($_GET['action'] == "update") {
-        echo '<form action="address.php" mehod="post">'.
+        global $db;
+        $address = $db->query('SELECT * FROM Address WHERE AddressID = '.$_GET['AddressID']);
+        $address = $address->fetch();
+
+        echo '<div class="address">';
+        echo '<form action="address.php" mehod="post" class="AddressForm">'.
             '<input type="hidden" name="AddressID" placeholder="AddressID" id="AddressID" value="'. $_GET['AddressID'] .'" required>'.
-            '<label>Street</label>'.
-            '<input type="text" name="Street" placeholder="Street" id="Street" required>'.
+            '<label>Street<label/>'.
+            '<input type="text" name="Street" placeholder="Street" id="Street" required value="'.$address["Street"].'">'.
             '<label>City</label>'.
-            '<input type="text" name="City" placeholder="City" id="City" required>'.
+            '<input type="text" name="City" placeholder="City" id="City" required value="'.$address["City"].'">'.
             '<label>State</label>'.
-            '<input type="text" name="State" placeholder="State" id="State" required>'.
+            '<input type="text" name="State" placeholder="State" id="State" required value="'.$address["State"].'">'.
             '<label>Zip</label>'.
-            '<input type="text" name="Zip" placeholder="Zip" id="Zip" required>'.
+            '<input type="text" name="Zip" placeholder="Zip" id="Zip" required value="'.$address["Zip"].'">'.
             '<label>Country</label>'.
-            '<input type="text" name="Country" placeholder="Country" id="Country" required>'.
-            '<input type="submit" value="Update">'.
+            '<input type="text" name="Country" placeholder="Country" id="Country" required value="'.$address["Country"].'">'.
+            '<input type="submit" value="Update" class="update">'.
             '</form>';
-        echo '<a href="profile.php">Cancel</a>';
+        echo '<a href="profile.php" class="cancel">Cancel</a></div>';
     } else if ($_GET['action'] == "create") {
-        echo '<form action="address.php" method="post">'.
-            '<label>Street</label>'.
+        echo '<div class="address">';
+        echo '<form action="address.php" method="post" class="AddressForm">'.
+            '<label>Street<label/>'.
             '<input type="text" name="Street" placeholder="Street" id="Street" required>'.
             '<label>City</label>'.
             '<input type="text" name="City" placeholder="City" id="City" required>'.
@@ -35,8 +53,9 @@ if(isset($_GET['action'])) {
             '<input type="text" name="Zip" placeholder="Zip" id="Zip" required>'.
             '<label>Country</label>'.
             '<input type="text" name="Country" placeholder="Country" id="Country" required>'.
+            '<input type="submit" value="Add" class="create">'.
             '</form>';
-        echo '<a href="profile.php">Cancel</a>';
+        echo '<a href="profile.php" class="cancel">Cancel</a></div>';
     } else {
         // unknown GET action
         header('Location: profile.php');
@@ -49,3 +68,6 @@ if(isset($_GET['action'])) {
     // nothing to do, sending back to profile screen
     header('Location: profile.php');
 }
+?>
+</body>
+</html>
