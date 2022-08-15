f4771390cf5f66993fde57d466e7d494d42a30b4
Ken <ken_chief_engineer@yahoo.com>
2022-08-14 22:17:17 -0400

    fix details page to handle being passed bad productid values

--- details.php
+++ details.php
@@ -6,17 +6,21 @@ $prod_id = $_GET['ProductID'];
 if($prod_id) {
     $p = $db->query('SELECT * FROM products WHERE ProductID =' . $_GET['ProductID']);
     $product = $p->fetch();
-//    print_r($product);
-    echo '<h3>'.$product["Name"].'</h3>';
-    echo '<hr /><p>'.$product["Details"].'</p>';
-    echo '<hr /><h3>'.$product["Cost"].'</h3>';
-    if(isset($_SESSION['id'])) {
-        echo'<form action="add2cart.php?ProductID='.$prod_id.'">'.
-            '<input type="submit" value="Add to Cart">'.
-        '</form>';
+    if($product) {
+        echo '<h3>' . $product["Name"] . '</h3>';
+        echo '<hr /><p>' . $product["Details"] . '</p>';
+        echo '<hr /><h3>' . $product["Cost"] . '</h3>';
+        if (isset($_SESSION['id'])) {
+            echo '<form action="add2cart.php">' .
+                '<input type="hidden" name="ProductID" value=' . $product["ProductID"] . '>' .
+                '<input type="submit" value="Add to Cart">' .
+                '</form>';
+        }
+    } else {
+        echo '<h3>ProductID '. $_GET['ProductID'] .' not found</h3>';
     }
 } else {
-    echo '<script type=javascript>alert("ProductID not found");</script>';
+    echo '<h3>ProductID not specified</h3>';
 }
 
 echo '<hr /><a href="./index.php">CLOSE</a>';
