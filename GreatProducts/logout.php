7b1281888d443301a38e6d6601dd7c1017e6be63
Ken <ken_chief_engineer@yahoo.com>
2022-08-15 22:29:21 -0400

    Clean out  unused duplicate copies of old stuff

--- GreatProducts/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// This file just terminates the users curretn session on logout then redirects them to the general user area.
-session_start();
-session_destroy();
-// Redirect to the landing page:
-header('Location: index.php');
-?>
