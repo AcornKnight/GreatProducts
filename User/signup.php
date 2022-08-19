ba84bcc920ae1a81f51bfc385869a36edee9ca11
Ken <ken_chief_engineer@yahoo.com>
2022-08-19 01:40:06 -0400

    tweaks to signup

--- User/signup.php
+++ User/signup.php
@@ -1,27 +1,23 @@
 <?php
 	require_once(__DIR__.'/../Utils/settings.php');
-	require_once(__DIR__.'/../Utils/utils.php'); ?>
+	require_once(__DIR__.'/../Utils/utils.php');
+    guard("guest");
+?>
 <!DOCTYPE html>
 <!-- Sign-in page for our Great products database. -->
-<!-- Noah R Gestiehr. This page should display a login form to the user and authenticate it -- >
+<!-- Noah R Gestiehr. This page should display a login form to the user and authenticate it -->
 <!-- On successful authentication, should redirect user to the landing page. Otherwise errors -->
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Sign-up</title>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-    <link href="../style.css" rel="stylesheet" type="text/css">
-
-    $con = mysqli_connect($host, $username, $password, $dbname);
-    if ( mysqli_connect_errno() ) {
-     // If there is an error with the connection, stop the script and display the error.
-     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-    }
+        <link href="../style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<div class="login">
 			<h1>Sign-up</h1>
-			<form action="./User/resgister.php" method="post">
+			<form action="./register.php" method="post">
 				<label for="username">
 					<i class="fas fa-user"></i>
 				</label>
